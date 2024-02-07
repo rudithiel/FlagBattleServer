043f623ae3aee402b7fef2a89e310865e1bf2906
Rudi Thiel <rudi@rudithiel.com>
2024-02-07 15:42:23 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import { Server as socketIo } from 'socket.io';
 import cors from 'cors';
 import fetch from 'node-fetch'; // fetch is already using import
 import { v4 as uuidv4 } from 'uuid';
+import { start } from 'repl';
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -21,44 +22,118 @@ const io = new socketIo(server, {
 });
 
 let users = {};
-let scores = { player1: 0, player2: 0 };
+let questions = [];
+let scores = {};
 let currentRound = 0;
+let roundActive = false;
+let userOrder = [];
 const totalRounds = 10;
 
-async function fetchQuestion() {
-    const response = await fetch('https://opentdb.com/api.php?amount=1&type=multiple');
+async function fetchQuestions() {
+    const response = await fetch('https://opentdb.com/api.php?amount=10&type=multiple');
     const data = await response.json();
-    return data.results[0];
+    console.log(data);
+    return data.results;
 }
 
 io.on('connection', (socket) => {
     console.log('Client connected');
-
-    socket.on('getUserId', async (userId) => {
-        if (!users[userId]) {
+    socket.emit('getUserId');
+    emitQuestion();
+    socket.on('getUserId', (userIdCookie) => {
+        console.log('User ID: ' + userIdCookie);
+        let userId = userIdCookie.replace('userId=', '');
+        if (users[userId] === undefined ) {
             userId = uuidv4();
-            users[userId] = { score: 0, username: "Anonymous" };
+            // Set the user ID cookie with a 1 year expiry
             socket.emit('setUserId', userId);
+            console.log('Prompting for username');
+            users[userId] = {
+                username: "Anonymous",
+                score: 0
+            };
+            // Prompt  the user for a username
+            socket.emit('setUsername');
+            userOrder.push(userId);
+            // Check if user has score, else set to 0
+            if (scores[userId] === undefined) {
+                scores[userId] = 0;
+            }
+            console.log("userOrder: " + userOrder);
         }
+        console.log(users[userId]);
+        socket.userId = userId;
+        console.log('Client connected with username ' + users[userId].username);
+    });
 
-        if (Object.keys(users).length === 2 && currentRound === 0) {
-            startGame();
+    socket.on('setUsername', (username) => {
+        // Check that username is not empty and that it is not already taken
+        if (username === "" || Object.values(users).some(user => user.username === username)) {
+            socket.emit('setUsername');
+            return;
         }
+        console.log(`Username set: ${username}`);
+        users[socket.userId].username = username;
+        io.emit('users', users);
+        console.log(users);
+        startGame();
+    });
+
+    socket.on('answer', (answer) => {
+        // Evaluate answer and update scores
+        // This is a simplified logic, you'll need to implement the answer handling and scoring
+        console.log('Answer received: ' + answer);
+        console.log('Correct answer: ' + questions[currentRound].correct_answer);
+
+        if ((answer === questions[currentRound].correct_answer) && roundActive) {
+            roundActive = false;
+            console.log('Correct answer!');
+            scores[socket.userId] += 1;
+            io.emit('correctUser', users[socket.userId].username); // Notify all users that the answer was correct
+            io.emit('correctAnswer', answer); // Notify all users that the answer was correct
+            nextRoundCounter();
+        } else if (roundActive) {
+            scores[socket.userId] -= 1;
+            console.log('Wrong answer');
+            socket.emit('wrongAnswer', answer); // Notify the specific user that the answer was wrong
+            io.emit('disableAnswer', answer); // Notify all users to disable this answer
+        }
+        console.log(scores);
+        io.emit('users', users);
     });
 
     async function startGame() {
-        for (currentRound = 1; currentRound <= totalRounds; currentRound++) {
-            const question = await fetchQuestion();
-            io.emit('newQuestion', question);
-            // Wait for both users to submit their answers and then evaluate
-            // This is a simplified logic, you'll need to implement the answer handling and scoring
+        const questions = await fetchQuestions();
+        roundActive = true;
+    }
+
+    function emitQuestion() {
+        // If questions is empty, fetch new questions
+        if (questions.length === 0) {
+            fetchQuestions().then((data) => {
+                questions = data;
+                console.log(questions);
+                io.emit('newQuestion', questions[currentRound]);
+            });
+        } else {
+            io.emit('newQuestion', questions[currentRound]);
         }
-        // After 10 rounds, determine winner and reset game
-        console.log('Game Over');
-        // Send game over message with scores
-        io.emit('gameOver', scores);
-        // Reset game state for next match
-        resetGame();
+    }
+
+    function nextRoundCounter() {
+        // Emit a 5 second countdown to the next round
+        let counter = 5;
+        const interval = setInterval(() => {
+            io.emit('nextRoundCounter', counter);
+            counter--;
+            if (counter < 0) {
+                clearInterval(interval);
+                currentRound++;
+                roundActive = true;
+                io.emit('newRound');
+                emitQuestion();
+            }
+        }, 1000);
     }
 
     function resetGame() {
